c2fe788665755849f03262f0d4e7af948c99901e
QASAK <w663012911@outlook.com>
2020-10-11 17:23:12 +0800

    update

---
+++ mall/src/test/java/com/imooc/mall/dao/OrderMapperTest.java
@@ -0,0 +1,50 @@
+package com.imooc.mall.dao;
+
+import org.junit.jupiter.api.Test;
+
+class OrderMapperTest {
+
+    @Test
+    void countByExample() {
+    }
+
+    @Test
+    void deleteByExample() {
+    }
+
+    @Test
+    void deleteByPrimaryKey() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void insertSelective() {
+    }
+
+    @Test
+    void selectByExample() {
+    }
+
+    @Test
+    void selectByPrimaryKey() {
+    }
+
+    @Test
+    void updateByExampleSelective() {
+    }
+
+    @Test
+    void updateByExample() {
+    }
+
+    @Test
+    void updateByPrimaryKeySelective() {
+    }
+
+    @Test
+    void updateByPrimaryKey() {
+    }
+}
